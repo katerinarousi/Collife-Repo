a3677541fea0af0a698005d46d416ea53ad7330d
zoispyridi <115904667+zoispyridi@users.noreply.github.com>
2022-12-10 15:09:42 +0200

    Add files via upload

--- Like.java
+++ Like.java
@@ -1,30 +1,52 @@
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
 
 public class Like {
+	//count LIKE
 
-	private String username;
-	private String msg;
+	private String url = "C:\\Users\\paras\\eclipse-workspace\\SQLiteConnection\\cc_db.db\\";
+	Connection connection = null;
+	 PreparedStatement state = null;
+		    public void InsertLike(int likeid,String name,int msgid) {
+				String insertSql = "INSERT INTO Likes (like_id,username,msg_id) VALUES ( " + likeid + " ," + name + "," + msgid + ");";
+			    ResultSet resultSet = null;
+			    try {
+				  connection = DriverManager.getConnection(url);
+				  state =connection.prepareStatement(insertSql);
 
-	public Like(String username, String msg) {
-		this.username = username;
-		this.msg = msg;
+				  state.execute();
 
+		        } catch (Exception e) {
+				  e.printStackTrace();
+			    }
 	}
-	/* σε περίπτωση που ο χρήστης δηλώσει πως θέλει να κάνει λαικ(κουμπί ή πληκτρολόγηση), η μειν καλεί τη λαικ στέλνοντάς της το ποστ το οποίο
-	 * επέλεξε ο χρήστης καθώς και τη θέση του στη βάση. αυτή καταχωρεί το λαικ, αυξάνοντας τον αριθμό των λαικς που έχει το ποστ κατα 1.
-	 */
 
-    public int countLike(String msg) {
-		ResultSet resultset = null;
+	public int countLike(int msgid) {
+					String sSql = " SELECT * FROM Likes WHERE msg_id  LIKE ?; ";
+				    ResultSet resultSet = null;
+				    try {
+					  connection = DriverManager.getConnection(url);
+					  state =connection.prepareStatement(sSql);
+					  resultSet = state.executeQuery(sSql);
 
-	    String selectSql = " SELECT like_id , msg_id from Collife.Like where msg_id = msg";
-	    resultSet = statement.executeQuery(selectSql);
+					  int count = 0;
+					  	    //count like and print
+					  	    while (resultSet.next()) {
+					  			count++;
+					  			System.out.println(resultSet.getInt("like_id"));
+					  			System.out.println(resultSet.getInt("msg_id"));
 
-	    int count = 0;
-	    while ( resultSet.next()) {
-		      count++;
-	    }
-        return count;
+					  		}
+                      return count;
+
+			        } catch (Exception e) {
+					  e.printStackTrace();
+					  return -1;
+				    }
+
 	}
 }
