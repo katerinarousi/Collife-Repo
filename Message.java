6c67a4e7193dfa166acb3572ab664e1787cc6697
zoispyridi <115904667+zoispyridi@users.noreply.github.com>
2022-12-11 20:31:38 +0200

    Update Message.java

--- Message.java
+++ Message.java
@@ -4,11 +4,48 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.PreparedStatement;
+import java.util.ArrayList;
 
 public class Message {
 
 	private String url = "C:\\Users\\paras\\eclipse-workspace\\SQLiteConnection\\cc_db.db\\";
-    public void InsertMess(int id,String name,String des,int category) {
+
+    public String getMessage(ArrayList<String> i) {
+		return i.get(i.size());
+	}
+
+	public String getID(int count ,String category) {
+		String myID;
+		count--;
+		switch (category) {
+			case "GENERAL":
+			   myID = "1." + count;
+               break;
+			case "VOLUNTEER":
+			   myID = "2." + count;
+               break;
+			case "NIGHTLIFE":
+			   myID =  "3." + count;
+               break;
+			case "TRAVEL":
+               myID =  "4." + count;
+			   break;
+			case "FOOD":
+			   myID =  "5." + count;
+               break;
+            case "UNIVERSITY":
+			   myID = "6." + count;
+               break;
+			default:
+			   myID =  "-1";
+
+		 }
+		 return myID;
+	 }
+
+
+
+    public void insertMessage(String id,String name,String des,String category) {
 		String insertSql = "INSERT INTO Message (msg_id,username,description,category_id) VALUES ( " + id + " ," + name + "," + des + "," + category + ");";
 	    ResultSet resultSet = null;
 	    try {
