39218982e30aea4043dc8e1f6247bd38d0128c41
kyriakip <115928532+kyriakip@users.noreply.github.com>
2023-01-10 13:18:32 +0200

    Update SQLiteConnectionB.java

--- CollifeFinal/src/SQLiteConnectionB.java
+++ CollifeFinal/src/SQLiteConnectionB.java
@@ -8,6 +8,8 @@ public class SQLiteConnectionB {
      Connection conn = null;
      public static Connection ConnectDb() {
      
+     //Declare JDBC Connection
+	    	     
      try {
     	 Class.forName("org.sqlite.JDBC");
     	 Connection conn=DriverManager.getConnection("jdbc:sqlite:cc_db.db");
